acc1184f896c1c0a0ee8c018fb7408c4387d39f2
yury <shubin1978@omnicommdv.ru>
2023-09-29 11:21:37 +0800

    Добавление класса Unit

---
+++ Model/Unit.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectApp.Model;
+
+public record Unit
+{
+    public int Id { get; init; }
+    public string Cathegory { get; init; }
+
+    public Unit(int id, string cathegory)
+    {
+        Id = id;
+        Cathegory = cathegory;
+    }
+}

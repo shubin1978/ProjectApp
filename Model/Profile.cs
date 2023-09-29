fd5477dae105f93220446a9d8b8f4dcc2ef639c8
yury <shubin1978@omnicommdv.ru>
2023-09-29 11:21:01 +0800

    Добавлен класс Profile

---
+++ Model/Profile.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectApp.Model;
+
+public record Profile
+{
+    public int At { get; init; }
+    public string Title { get; init; }
+    public string Position { get; init; }
+    public Status Status { get; init; }
+}

39795985f1365468e0dafe900ce0f5428c13c3ba
yury <shubin1978@omnicommdv.ru>
2023-10-02 21:45:08 +0800

    Добавил интерфейс IContext

---
+++ DAL/IContext.cs
@@ -0,0 +1,11 @@
+﻿using Object = ProjectApp.Model.Object;
+
+namespace ProjectApp.DAL;
+
+public interface IContext
+{
+    public List<Object> Objects { get; set; }
+
+    public void ImportObjects();
+    public void ExportObjects();
+}

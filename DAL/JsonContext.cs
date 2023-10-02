89b930cc3dbd6ed765301252196463c4e90aed22
yury <shubin1978@omnicommdv.ru>
2023-10-02 21:46:25 +0800

    Добавил класс JsonContext

---
+++ DAL/JsonContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using Object = ProjectApp.Model.Object;
+
+namespace ProjectApp.DAL;
+
+public class JsonContext : FileContext, IContext
+{
+    public List<Object> Objects { get; set; }
+    private readonly JsonSerializerOptions _options;
+    
+    public JsonContext(string pathToObjects) : base(pathToObjects)
+    {
+        Objects = new List<Object>();
+        
+        _options = new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+    }
+    
+    public void ImportObjects()
+    {
+        var objectJson = File.ReadAllText(PathToObjects);
+        Objects = JsonSerializer.Deserialize<List<Object>>(objectJson);
+    }
+
+    public void ExportObjects()
+    {
+        var objectsJson = JsonSerializer.Serialize(Objects, _options);
+        File.WriteAllText(PathToObjects,objectsJson);
+    }
+}

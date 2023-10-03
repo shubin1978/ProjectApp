92278f0a3773bd32f7402f2bf8b0d7b411ca7125
yury <shubin1978@omnicommdv.ru>
2023-10-03 12:19:48 +0800

    Добавление уровня взаимодействия с пользователем

--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.Text.Json;
+using ProjectApp.BLL;
+using ProjectApp.CLI;
+using ProjectApp.DAL;
+using ProjectApp.Model;
+using Object = ProjectApp.Model.Object;
 
-Console.WriteLine("Hello, World!");
+var company = new Company(new JsonContext("objects.json"));
+/*context.Objects = new List<Object>
+{
+ new()
+ {
+  At = 326001010,
+  Title = "PC-400_#605",
+  Position = "Quarry",
+  Status = Status.Avtive,
+  Equipment = new List<Unit>
+  {
+   new Unit(2222, "LLS4"),
+
+   new Unit(2223, "LLS4")
+  }
+ }
+};*/
+
+var objects = company.GetAllObjects();
+CompanyView.ShowObjects(objects);
+var position = CLI.InputString("Введите локацию для поиска");
+var listObjectsLocation = company.FindObjectByPlace(position);
+CompanyView.ShowObjects(listObjectsLocation);
